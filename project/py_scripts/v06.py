753975128f47d637227091cf8475a6525af93361
Ryan Cai <ryan.cai3@education.nsw.gov.au>
2025-08-12 13:20:43 +1000

    More Stuff

--- project/py_scripts/v06.py
+++ project/py_scripts/v06.py
@@ -2,12 +2,10 @@ from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
 from PiicoDev_Unified import sleep_ms
 
 range_a = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-range_b = PiicoDev_Ultrasonic(
-    id=[1, 0, 0, 0]
-)  # id argument must match ID switch positions
+range_b = PiicoDev_Ultrasonic(id=[0, 0, 1, 0])  # id argument must match ID switch positions
 
 while True:
 
-    print(range_a.distance_mm, range_b.distance_mm)
+    print(f"{range_a.distance_mm}, {range_b.distance_mm}")
 
     sleep_ms(100)
