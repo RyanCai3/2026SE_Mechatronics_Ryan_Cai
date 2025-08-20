b9d663b8fa8b8bac73a48b93cff1fcfb105c6fbd
Ryan C <ryan.cai3@education.nsw.gov.au>
2025-08-21 00:49:24 +1000

    Finally

---
+++ py_tests/detection_test.py
@@ -0,0 +1,25 @@
+from PiicoDev_Unified import sleep_ms
+from controller import DetectionSubsystem
+from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
+
+f_sensor = PiicoDev_Ultrasonic(id=[0,0,0,0])
+r_sensor = PiicoDev_Ultrasonic(id=[0,0,1,0])
+
+threshold_1_mm = 200
+threshold_2_mm = 50
+
+detection = DetectionSubsystem(f_sensor, r_sensor, threshold_1_mm, threshold_2_mm)
+
+while True:
+    print(f"Front Sensor: {detection.get_distance_sensor_1()} mm")
+    print(f"Right Sensor: {detection.get_distance_sensor_2()} mm")
+    sleep_ms(500)
+
+    if detection.sensor_1_obstacle_detected():
+        print("Obstacle Detected In Front!")
+
+    if detection.sensor_2_obstacle_detected():
+        print("Obstacle Detected On Right!")
+
+    if not detection.any_obstacle():
+        print("Path Clear!")
